c996b2c3bdb4e11cbc9f770330042361139be63a
scrappy-cyborg-4243 <itchkimel@gmail.com>
2020-10-28 21:13:45 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,16 +1,1 @@
 # Use this space to code along with the readme
-def
-say_hello_world_ten_times
-
-  phrase = "Hello World!"
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-end
